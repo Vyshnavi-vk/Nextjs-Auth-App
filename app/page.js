806f519841eb6e001ddfade56350f81e50acac7c
Vyshnavi-vk <kuntumallavyshnavi@gmail.com>
2024-01-24 12:54:30 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,20 @@
+'use client'
+import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+
+
+export default function Home() {
+  const { data: session } = useSession()
+
+  return !session ? (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Link href='/login' className='border px-4 py-2 text-red-400 rounded'>
+        Please login to see the content
+      </Link>
+    </div>
+  ) : (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <p className='text-green-400 border p-5'>Welcome back buddy</p>
+    </div>
+  )
+}
